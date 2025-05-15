3526d88b0b361396d68fd3bbef55ba70d6233e29
PI01212 <sofiasofia9829303040401@gmail.com>
2025-05-15 19:40:53 +0800

    Delete Source directory

--- Source/BlasterLearing/BlasterLearing.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BlasterLearing.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlasterLearing, "BlasterLearing" );
--- Source/BlasterLearing/BlasterLearing.h
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
-#define ECC_SkeletalMesh ECollisionChannel::ECC_GameTraceChannel1
-#define ECC_HitBox ECollisionChannel::ECC_GameTraceChannel2
